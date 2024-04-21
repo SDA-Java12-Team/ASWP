727b571ee910a2bdddfb080dc6da90ac136ead29
Ív <ivusavackova@seznam.cz>
2024-04-21 18:52:14 +0200

    add methods

--- src/main/java/cz/coffee/controllers/TeamsController.java
+++ src/main/java/cz/coffee/controllers/TeamsController.java
@@ -1,4 +1,52 @@
 package cz.coffee.controllers;
 
 public class TeamsController {
+
+    private String subject;
+
+    private int startDateTime;
+
+    private int endDateTime;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(int startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public int getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(int endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+
+
+    public TeamsController(String subject, int startDateTime, int endDateTime) {
+        this.subject = subject;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
+
+
+
+    public static void createMeeting() {
+        System.out.println();
+    }
+
+    public static void deleteMeeting(){
+
+    }
 }
