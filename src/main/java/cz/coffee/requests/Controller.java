b8ecde5773002643c0bc3b7659f1988eb6e6aefe
Qve <urna68+git@gmail.com>
2024-04-21 18:39:23 +0200

    add method

--- src/main/java/cz/coffee/requests/Controller.java
+++ src/main/java/cz/coffee/requests/Controller.java
@@ -7,21 +7,38 @@ public class Controller {
     private String client_id;
     private String state;
     private String redirect_uri;
+    private String clientSecret;
+    private String apiKey;
 
-    public boolean GET(){
-        return false;
+    public Access GET(){
+        return Access.UNKNOWN;
     }
-    public boolean POST(){
-        return false;
+    public Access POST(){
+        return Access.UNKNOWN;
     }
 
     public Access PUT(){
         return Access.UNKNOWN;
     }
 
-
+    public Access UPDATE(){
+        return Access.UNKNOWN;
+    }
+    public Access DELETE(){
+        return Access.UNKNOWN;
+    }
+    public Access PATCH(){
+        return Access.UNKNOWN;
+    }
+    public Access OPTION(){
+        return Access.UNKNOWN;
+    }
+    public Access HEAD(){
+        return Access.UNKNOWN;
+    }
 
     public String httpsStringConstructor(){
+        //https://ib.airbank.cz/?client_id=MYID&response_type=code&state=ehshvnajgtf34
         return "";
     }
 }
