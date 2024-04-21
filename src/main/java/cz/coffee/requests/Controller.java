cc410613b11670a4916189d5bc6de463835e4401
Qve <urna68+git@gmail.com>
2024-04-21 18:24:02 +0200

    add class Controller in request package

---
+++ src/main/java/cz/coffee/requests/Controller.java
@@ -0,0 +1,20 @@
+package cz.coffee.requests;
+
+public class Controller {
+    private String urlBank;
+    private String client_id;
+    private String state;
+    private String redirect_uri;
+    public boolean GET(){
+
+    }
+    public boolean POST(){
+
+    }
+
+
+
+    public String httpsStringConstructor(){
+
+    }
+}
