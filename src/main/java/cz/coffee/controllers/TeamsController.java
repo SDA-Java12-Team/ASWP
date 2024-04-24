3d8a942d2a744806a35074939792404f78a367ec
Stefan <stefan.pocsik@gmail.com>
2024-04-24 23:49:55 +0200

    completly refactored class. Iv, you can complete a todo tasks in controller.

--- src/main/java/cz/coffee/controllers/TeamsController.java
+++ src/main/java/cz/coffee/controllers/TeamsController.java
@@ -1,51 +1,136 @@
 package cz.coffee.controllers;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 public class TeamsController {
 
+    private String meetingID;
+
     private String subject;
 
-    private LocalDateTime startDateTime;
+    private ZonedDateTime startDateTime;
 
-    private LocalDateTime endDateTime;
+    private ZonedDateTime endDateTime;
 
-    public TeamsController(String subject) {
-        this.subject = subject;
-        this.startDateTime = LocalDateTime.now();
+    private LocalTime duration;
+
+    private String[] participants;
+
+    private int participantsCount;
+
+    Random random = new Random();
+
+    public TeamsController(String meetingID) {
+        //
+        // this class will be called like:
+        // TeamController teamController = new TeamController("ds654sdf456dsf5ds4f6");
+        //
+        // TODO make a constructor and add meetingID to the fields
+        // TODO fill the rest fields from methods below (subject, startDateTime, endDateTime, participants)
+        // TODO make a methods to fill the duration and participantsCount fields
+
+        System.out.println(createSubject());
     }
 
 
-    public String getSubject() {
-        return subject;
+    private String[] createParticipants(int number) {
+
+        if (number < 1) return null;
+
+        String[] participants = new String[number];
+
+        for (int i = 0; i < participants.length; i++) {
+
+            int responseCode;
+            InputStream inputStream;
+            try {
+                URL url = URI.create("https://api.namefake.com/").toURL();
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                inputStream = conn.getInputStream();
+
+                responseCode = conn.getResponseCode();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            BufferedReader br = null;
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            if (responseCode != 200) {
+                return null;
+            }
+
+            try {
+                br = new BufferedReader(new InputStreamReader(inputStream));
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            JsonObject jobj = new Gson().fromJson(sb.toString(), JsonObject.class);
+            participants[i] = jobj.get("name").toString();
+        }
+        return participants;
     }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
+    private ZonedDateTime createEndDateTime() {
+        int hours = this.random.nextInt(24);
+        int minutes = this.random.nextInt(60);
+
+        return startDateTime.minusHours(hours).minusMinutes(minutes);
     }
 
-    public LocalDateTime getStartDateTime() {
-        return startDateTime;
+    private ZonedDateTime createStartDateTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'");
+
+        return ZonedDateTime.parse(formatter.format(ZonedDateTime.now()));
     }
 
-    public void setStartDateTime(LocalDateTime startDateTime) {
-        this.startDateTime = startDateTime;
-    }
+    private String createSubject() {
 
-    public LocalDateTime getEndDateTime() {
-        return endDateTime;
-    }
+        // random integer between 2 - 6
+        int wordsLength = this.random.nextInt(5) + 2;
+        // random integer between 2 - 4
+        int numberOfWords = this.random.nextInt((4 - 2) + 1) + 2;
+        String result = "";
 
-    public void setEndDateTime(LocalDateTime endDateTime) {
-        this.endDateTime = endDateTime;
-    }
+        for (int i = 0; i < numberOfWords; i++) {
+            String word = "";
+            for (int j = 0; j < wordsLength; j++) {
+                char ch = (char) (random.nextInt(26) + 'a');
 
-    public static void createMeeting() {
-        System.out.println("The meeting is created");
-    }
+                if (j == 0) {
+                    ch = Character.toUpperCase(ch);
+                }
 
-    public static void deleteMeeting(){
+                word = word.concat(String.valueOf(ch));
+            }
+            result = result.concat(word + " ");
+        }
 
+        return result.trim();
     }
 }
