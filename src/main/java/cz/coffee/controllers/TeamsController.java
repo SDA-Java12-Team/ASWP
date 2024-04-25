777dda1fdedbf3fc58a068c113e0bdb6ac673ba8
Ív <ivusavackova@seznam.cz>
2024-04-25 21:37:17 +0200

    create Subject

--- src/main/java/cz/coffee/controllers/TeamsController.java
+++ src/main/java/cz/coffee/controllers/TeamsController.java
@@ -1,7 +1,7 @@
 package cz.coffee.controllers;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+//import com.google.gson.Gson;
+//import com.google.gson.JsonObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -33,68 +33,71 @@ public class TeamsController {
 
     Random random = new Random();
 
+
+
     public TeamsController(String meetingID) {
         //
         // this class will be called like:
         // TeamController teamController = new TeamController("ds654sdf456dsf5ds4f6");
         //
-        // TODO make a constructor and add meetingID to the fields
         // TODO fill the rest fields from methods below (subject, startDateTime, endDateTime, participants)
         // TODO make a methods to fill the duration and participantsCount fields
-
-        System.out.println(createSubject());
+            this.meetingID = meetingID;
+            this.subject = createSubject();
+    }
+    public String getSubject() {
+        return subject;
     }
 
-
-    private String[] createParticipants(int number) {
-
-        if (number < 1) return null;
-
-        String[] participants = new String[number];
-
-        for (int i = 0; i < participants.length; i++) {
-
-            int responseCode;
-            InputStream inputStream;
-            try {
-                URL url = URI.create("https://api.namefake.com/").toURL();
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                inputStream = conn.getInputStream();
-
-                responseCode = conn.getResponseCode();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            BufferedReader br = null;
-            StringBuilder sb = new StringBuilder();
-            String line;
-
-            if (responseCode != 200) {
-                return null;
-            }
-
-            try {
-                br = new BufferedReader(new InputStreamReader(inputStream));
-                while ((line = br.readLine()) != null) {
-                    sb.append(line);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                if (br != null) {
-                    try {
-                        br.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            JsonObject jobj = new Gson().fromJson(sb.toString(), JsonObject.class);
-            participants[i] = jobj.get("name").toString();
-        }
-        return participants;
-    }
+//    private String[] createParticipants(int number) {
+//
+//        if (number < 1) return null;
+//
+//        String[] participants = new String[number];
+//
+//        for (int i = 0; i < participants.length; i++) {
+//
+//            int responseCode;
+//            InputStream inputStream;
+//            try {
+//                URL url = URI.create("https://api.namefake.com/").toURL();
+//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                inputStream = conn.getInputStream();
+//
+//                responseCode = conn.getResponseCode();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//            BufferedReader br = null;
+//            StringBuilder sb = new StringBuilder();
+//            String line;
+//
+//            if (responseCode != 200) {
+//                return null;
+//            }
+//
+//            try {
+//                br = new BufferedReader(new InputStreamReader(inputStream));
+//                while ((line = br.readLine()) != null) {
+//                    sb.append(line);
+//                }
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            } finally {
+//                if (br != null) {
+//                    try {
+//                        br.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//            JsonObject jobj = new Gson().fromJson(sb.toString(), JsonObject.class);
+//            participants[i] = jobj.get("name").toString();
+//        }
+//        return participants;
+//    }
 
     private ZonedDateTime createEndDateTime() {
         int hours = this.random.nextInt(24);
